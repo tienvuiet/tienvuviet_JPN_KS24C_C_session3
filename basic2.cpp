084231f52c6134336b19ec95719d943a810e13ad
tienvuiet <tienxinhzai241@GMAIL.COM>
2024-11-20 21:11:30 +0700

    basic2

---
+++ basic2.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h> 
+int main(){
+    float Celsius;
+	printf("nhap vao do celsius: ");
+	scanf("%f",&Celsius); 
+	float Fahrenheit=Celsius*1.8+32;
+	printf("ket qua cua chuyen doi la: %f", Fahrenheit) ;
+	return 0; 
+	 
+} 

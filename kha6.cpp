ca0b89d898a07b2af4b71b5cd0286b21e114c88f
tienvuiet <tienxinhzai241@GMAIL.COM>
2024-11-20 22:10:50 +0700

    kha6

---
+++ kha6.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h> 
+int main(){
+    float dodaicanh;
+    float chieucao;
+	printf("nhap vao do dai canh: ");
+	scanf("%f",&dodaicanh ) ;
+	printf("nhap vao chieu cao: ");
+	scanf("%f",&chieucao ) ;
+    float dientich=0.5*dodaicanh*chieucao;
+	printf("dien tich tam giac :%f\n ",dientich);
+	return 0; 
+	 
+} 

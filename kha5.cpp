a1449c96123172d5c5d5c0f3712abf6f0dd79cdb
tienvuiet <tienxinhzai241@GMAIL.COM>
2024-11-21 09:22:54 +0700

    kha5

---
+++ kha5.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main (){
+	printf("--------------------------------------------------------\n");
+	printf("            DANH SACH SINH VIEN\n ");
+	printf("--------------------------------------------------------\n");
+	printf("STT |   HO VA TEN      |   SO DIEN THOAI |   EMAIL\n"), 
+	printf("--------------------------------------------------------\n");
+	printf(" 1  |   VU VIET TIEN   |   123957343     |   Tien@gmail.com\n"); 
+	printf("--------------------------------------------------------\n");
+	printf(" 2  |   VU TIEN VIET   |   15123218094   |   TienD?@gmail.com");
+
+	return 0;
+}
